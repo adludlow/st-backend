64f81030d3383001128fbbded73d176c722914ff
Adrian Ludlow <aludlow@gmail.com>
2018-08-16 22:11:26 +1000

    Initial login functionality.

--- app/server.js
+++ app/server.js
@@ -1,9 +1,77 @@
 const express = require('express');
+const rp = require('request-promise');
+const db = require('./db');
 
 const app = express();
 
 app.get('/health', (req, res) => {
-    req.send('OK');
+  req.send('OK');
+});
+
+app.post('/login', async (req, res) => {
+  try {
+    const creds = Buffer.from(
+      req.get('Authorization')
+        .substring(6), 'base64')
+        .toString('ascii');
+
+    const [username, password] = creds.split(':');
+
+    const accessTokenReq = {
+      method: 'POST',
+      uri: 'https://supercoach.heraldsun.com.au/api/afl/classic/v1/access_token',
+      body: {
+        grant_type: "password",
+        client_id: "Lf7m0371XCbMBlQ0fAFoGRJlfCs2JZpYvLU1uEvd",
+        client_secret:"",
+        username: username,
+        password: password
+      },
+      json: true
+    };
+
+    let tokenResponse = await rp(accessTokenReq);
+    let accessToken = tokenResponse.access_token;
+
+    let existingUser = await db.run_query('select * from local_user where username = $1', [username]);
+    if(existingUser.length > 0) {
+      console.log('user exists');
+    }
+    else {
+      console.log('Creating new user.');
+      const accountDetailsReq = {
+        method: 'GET',
+        uri: 'https://supercoach.heraldsun.com.au/api/afl/classic/v1/me',
+        auth: {
+          bearer: accessToken
+        },
+        json: true
+      };
+
+      let accountDetails = await rp(accountDetailsReq);
+      db.run_query('insert into local_user(username, sc_id) values($1, $2)', [username, accountDetails.id]);
+    }
+    
+    res.status(200).send({
+      username,
+      accessToken
+    });
+  }
+  catch (e) {
+    throw e;
+  }
+});
+
+// Error handler.
+app.use((err, req, res, next) => {
+  console.log(`**** ERROR: ${err}`);
+  console.log(err.stack);
+  var error = {
+      "errorDetail": `${err}`
+  }
+  var statusCode = 500;
+
+  res.status(statusCode).send(error);
 });
 
 app.listen(3000, () => console.log('Server started.'));
--- db/migrations/V2__initial_schema.sql
+++ db/migrations/V2__initial_schema.sql
@@ -1,7 +1,62 @@
+create table local_user (
+  id serial primary key,
+  sc_id numeric,
+  username text,
+  created_dt timestamp with time zone default now()
+);
+
+create table local_user_role (
+  id serial primary key,
+  role_name,
+  created_dt timestamp with time zone default now()
+);
+
+create table local_user_role_rel (
+  id serial primary key,
+  user_id integer references local_user(id),
+  role_id integer references local_user_role(id),
+  created_dt timestamp with time zone default now()
+);
+
+create table league (
+  id serial primary key,
+  sc_id numeric,
+  created_dt timestamp with time zone default now()
+);
+
+create table user_league_rel (
+  id serial primary key,
+  user_id integer references local_user(id),
+  league_id integer references league(id)
+);
+
 create table player (
   ID serial PRIMARY KEY,
+  sc_id numeric,
   NAME text,
   ATTRIBUTES jsonb
 );
 
+create table team (
+  ID serial PRIMARY KEY,
+  sc_id numeric,
+  NAME text,
+  ATTRIBUTES jsonb
+);
+
+create table player_team_rel (
+  ID serial primary key,
+  player_id integer references player(id),
+  team_id integer references team(id),
+  created_dt timestamp with time zone default now(),
+  end_dt timestamp with time zone
+);
+
 grant select, insert, update, delete on table player to ${connect_user};
+grant select, insert, update, delete on table team to ${connect_user};
+grant select, insert, update, delete on table player_team_rel to ${connect_user};
+grant select, insert, update, delete on table local_user to ${connect_user};
+grant select, insert, update, delete on table league to ${connect_user};
+grant select, insert, update, delete on table user_league_rel to ${connect_user};
+
+grant usage, select on all sequences in schema ${main_schema} to ${connect_user}
